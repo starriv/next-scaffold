bfa231aa7117f51592de6f55d59f0905d6bbad60
starriv <starriv.993@gmail.com>
2017-08-25 15:18:26 +0800

    删除测试数据

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,8 @@
 import React, { Component } from 'react';
-import { Provider } from 'mobx-react';
-import { initStore } from '../mobx/stores/index-store';
 import Index from '../mobx/containers/index/index';
 
 export default class Deploying extends Component {
-  static getInitialProps() {
-    const store = initStore();
-    return { iconLoading: store.iconLoading, lastUpdate: store.lastUpdate };
-  }
-  constructor(props) {
-    super(props);
-    this.store = initStore(props.iconLoading, props.lastUpdate);
-  }
-
   render() {
-    return (
-      <Provider store={this.store}>
-        <Index title="Index Page" linkTo="/" />
-      </Provider>
-    );
+    return <Index linkTo="/" />;
   }
 }
